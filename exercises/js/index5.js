d7a11b5db0039047e31f56f7ff4a4cbf44e53b1f
Mitul Patel <patel.mitul.m@gmail.com>
2022-01-27 18:26:27 -0600

    JS: Ex5

---
+++ exercises/js/index5.js
@@ -0,0 +1,11 @@
+var FirstName = "Mitul";
+var LastName = "Patel";
+var age = 37;
+var DateOfBirth = "11/09/1982";
+var Address= "302-3222222 Who cares";
+
+console.log(FirstName);
+console.log(LastName);
+console.log(age);
+console.log(DateOfBirth);
+console.log(Address);
