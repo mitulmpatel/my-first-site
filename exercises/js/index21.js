ef6a1ce233891a8599d51354de5737f9529bce64
Mitul Patel <patel.mitul.m@gmail.com>
2022-01-31 21:18:25 -0600

    JS: Ex:21

---
+++ exercises/js/index21.js
@@ -0,0 +1,7 @@
+let age=39;
+let phone=3061231234;
+let streetName='Pinehouse Dr';
+let streetNumber='180';
+console.log(`I'm ${age} years old`);
+console.log(`My phone number is: ${phone}`);
+console.log(`I live on ${streetNumber} ${streetName}`);
