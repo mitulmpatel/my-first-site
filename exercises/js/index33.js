c5bb11855b7df297b52c09a9924bbd427e7cc058
Mitul Patel <patel.mitul.m@gmail.com>
2022-02-01 14:40:11 -0600

    JS: Ex:33

---
+++ exercises/js/index33.js
@@ -0,0 +1,12 @@
+let userCount = 100;
+userCount += 5;
+console.log(`userCount: ${userCount}`);
+userCount -= 3;
+console.log(`userCount: ${userCount}`);
+userCount *= 2;
+console.log(`userCount: ${userCount}`);
+let menCount = userCount * 0.5;
+let womenCount = userCount * 0.5;
+
+console.log(`Users count: ${userCount}, ${menCount} & ${womenCount}`);
+
