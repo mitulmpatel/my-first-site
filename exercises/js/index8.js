68e3d4bdc24d72ccdf467b7a49a062f5c9fe24f2
Mitul Patel <patel.mitul.m@gmail.com>
2022-01-31 20:54:53 -0600

    JS: Ex:8

---
+++ exercises/js/index8.js
@@ -0,0 +1,11 @@
+const FirstName = "Mitul";
+const LastName = "Patel";
+const age = 37;
+const DateOfBirth = "11/09/1982";
+const Address= "302-3222222 Who cares";
+
+console.log("FirstName:", FirstName);
+console.log("LastName:", LastName);
+console.log("age:", age);
+console.log("DateOfBirth:", DateOfBirth);
+console.log("Address:", Address);
