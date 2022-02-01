0a4a594094f6eecc462d562947f11017c4014375
Mitul Patel <patel.mitul.m@gmail.com>
2022-01-31 22:21:55 -0600

    JS: Ex:25

---
+++ exercises/js/index25.js
@@ -0,0 +1,13 @@
+let superHeroName = 'Batman';
+let name = 'Bruce Wayne';
+let fly = false;
+let hasBatmobile = true;
+let life = 34;
+let freeTime = null;
+
+console.log(`superHeroName: ${superHeroName} and is: ` + typeof superHeroName);
+console.log(`name: ${name} and is: ` + typeof name);
+console.log(`fly: ${fly} and is: ` + typeof fly);
+console.log(`hasBatmobile: ${hasBatmobile} and is: ` + typeof hasBatmobile);
+console.log(`life: ${life} and is: ` + typeof life);
+console.log(`freeTime: ${freeTime} and is: ` + typeof freeTime);
