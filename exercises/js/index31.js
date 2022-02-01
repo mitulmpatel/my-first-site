d36cf22fbef6f77718dab8b313bb6a7245053f7c
Mitul Patel <patel.mitul.m@gmail.com>
2022-02-01 14:26:53 -0600

    JS: Ex:32

---
+++ exercises/js/index31.js
@@ -0,0 +1,5 @@
+let side1=10;
+let side2=20;
+let side3=30;
+let perimeter = side1 + side2 + side3;
+console.log(`perimeter of triangle with ${side1} ${side2} & ${side3} is ${perimeter}`);
---
+++ exercises/js/index32.js
@@ -0,0 +1,7 @@
+let height = 10;
+let base = 4;
+let perimeter = 2*height + 2*base;
+let area = height * base;
+
+console.log(`perimeter of rectangular with height ${height} & base ${base} is ${perimeter}`);
+console.log(`area of rectangular with height ${height} & base ${base} is ${area}`);
