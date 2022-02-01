a6daab4debe0dcf36545bc4ebacd6791944d8f6c
Mitul Patel <patel.mitul.m@gmail.com>
2022-02-01 14:06:53 -0600

    JS: Ex:27

---
+++ exercises/js/index27.js
@@ -0,0 +1,15 @@
+let firstNumber=20;
+let secondNumber=10;
+
+let add = firstNumber + secondNumber;
+let subtract = firstNumber - secondNumber;
+let multiply = firstNumber * secondNumber;
+let divide = firstNumber / secondNumber;
+let mod = firstNumber % secondNumber;
+
+console.log(`I add ${firstNumber} + ${secondNumber} and the result is ${add}`);
+console.log(`I subtract ${firstNumber} - ${secondNumber} and the result is ${subtract}`);
+console.log(`I multiply ${firstNumber} * ${secondNumber} and the result is ${multiply}`);
+console.log(`I divide ${firstNumber} / ${secondNumber} and the result is ${divide}`);
+console.log(`I mod ${firstNumber} % ${secondNumber} and the result is ${mod}`);
+
