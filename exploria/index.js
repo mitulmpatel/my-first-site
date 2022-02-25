deec17de59d3df05b563ce61258bc13b5bdf8452
Mitul Patel <patel.mitul.m@gmail.com>
2022-02-25 15:27:23 -0600

    Project Structur & Pages

---
+++ exploria/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+
+app.use(express.static('public'));
+
+
+/* 
+app.get("/", (req, res) => {
+    res.render("index", {});
+}); 
+*/
+
+// Routers
+const home = require('./src/home.js');
+const properties = require('./src/properties.js');
+
+app.use('/', home);
+app.use('/properties', properties);
+app.get("/about", (req, res) => {
+    res.render("static/about", {});
+});
+app.get("/contact", (req, res) => {
+    res.render("static/contact", {});
+});
+
+
+app.get('/error', function (req, res) {
+    res.status(500).send('Server error');
+});
+
+// FOR 404 - Broken URLs
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!")
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log("something bad happened", err);
+    }
+    console.log(`Server running on port ${port}`);
+});
---
+++ exploria/src/home.js
@@ -0,0 +1,11 @@
+// Create the express router to handle our home requests
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  // res.send('Welcome to our Site!');
+  // res.status(200).json({ name: 'Marta', age: '40'});
+  res.render("index", {});
+});
+
+module.exports = router;
